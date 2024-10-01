5a48aaf1bb418b998188a9af506a0d456c37af5e
LeonDev633 <leonardo.dev667622969@gmail.com>
2024-10-01 16:49:16 +0000

    Leonardo

--- Arquivo Leonardo.py
+++ Arquivo Leonardo.py
@@ -2,3 +2,23 @@ import os
 from dataclasses import dataclass
 os.system ("cls||clear")
 
+@dataclass
+class Dados:
+    salario : float
+    filhos : int
+
+def criando_arquivo(a,b):
+    with open(a,"a") as arquivo_dados:
+        for dado in b:
+            arquivo_dados.write(f"{dado.usuario}, {dado.senha}\n")
+    arquivo_dados.close()
+    print("\n=== Dados Salvos ===\n")
+
+def lendo_arquivo(a):
+    list_dados=[]
+    with open(a,"r") as arquivo_origem:
+        for linha in arquivo_origem:
+            usuario, senha = linha.strip().split(",")
+            list_dados.append(Dados(usuario=usuario, senha=int(senha)))
+    arquivo_origem.close()
+    return list_dados
