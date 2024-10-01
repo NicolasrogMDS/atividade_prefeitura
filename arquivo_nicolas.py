1139d4930c930baf8c1937216f705bef3bc1fb0b
Nicolas Roger <nramonteirods05@gmail.com>
2024-10-01 16:55:12 +0000

    salvar

---
+++ arquivo_nicolas.py
@@ -0,0 +1,10 @@
+import os
+os.system("cls || clear")
+from dataclasses import dataclass
+
+print("""
+Código | Descrição
+  1      Adicionar família
+  2      Exibir resultados
+  3      Sair
+""")
